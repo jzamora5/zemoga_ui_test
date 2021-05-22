02725240de1a274699f5385b8b8be373d51d1a43
jzamora5 <jzamora_5@yahoo.com>
2021-05-21 20:13:03 -0500

    ADD Redux Implementation and initialState of Application

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, compose } from 'redux';
+import reducer from './reducers';
 import App from './App';
 import './assets/styles/main.css';
+import initialState from './initialState.json';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, initialState, composeEnhancers());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+const reducer = (state, action) => {
+  switch (action.type) {
+    default:
+      break;
+  }
+  return state;
+};
+
+export default reducer;
